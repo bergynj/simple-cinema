10fa0f23753c5cd731df42375622648187fee09b
Nathanael Jochanan <bergy_nj@hotmail.com>
2015-05-25 09:49:36 +1000

    Refactor Session Schema -bergynj

--- database/migrations/2015_05_23_174504_create_mvsessions_table.php
+++ database/migrations/2015_05_23_174504_create_mvsessions_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 class CreateMvsessionsTable extends Migration {
 
 	/**
-	 * Run the migrations.
+	 * Create Session time schema to run  for the migrations.
 	 *
 	 * @return void
 	 */
@@ -15,25 +15,40 @@ class CreateMvsessionsTable extends Migration {
 		Schema::create('mvsessions', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->integer('cinema_id');
-			$table->integer('movie_id');
 			$table->timestamp('session_time');
 			$table->timestamps();
-			// $table->foreign('cinema_id')->references('id')->on('cinema');
-			// $table->foreign('movie_id')->references('id')->on('movies');
-			// $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
             // $table->softDeletes();
 		});
+
+        // create pivot table : cinema, mvsessions => cinema_mvsession
+        Schema::create('cinema_mvsession', function(Blueprint $table)
+        {
+			$table->integer('cinema_id')->unsigned()->index();
+			$table->integer('mvsesion_id')->unsigned()->index();
+			$table->timestamps();
+            $table->foreign('cinema_id')->references('id')->on('cinema')->onDelete('cascade');
+        });
+
+        // create pivot table : movies, mvsessions => movie_mvsession
+        Schema::create('movie_mvsession', function(Blueprint $table)
+        {
+			$table->integer('movie_id')->unsigned()->index();
+			$table->integer('mvsesion_id')->unsigned()->index();
+			$table->timestamps();
+			$table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+        });
 	}
 
 	/**
-	 * Reverse the migrations.
+	 * Drpo Session time tables to reverse the migrations.
 	 *
 	 * @return void
 	 */
 	public function down()
 	{
 		Schema::drop('mvsessions');
+		Schema::drop('cinema_mvsession');
+		Schema::drop('movie_mvsession');
 	}
 
 }
